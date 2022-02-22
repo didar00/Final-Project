128cf5da431ee9b41fb3d57cda36af89e894baec
Gamze Deniz <42394685+gamzedeniz@users.noreply.github.com>
2022-02-22 16:00:13 +0300

    final - working version for umap representation
    Will need adjustments for general use

--- umap_rep.py
+++ umap_rep.py
@@ -9,18 +9,17 @@ class_df = chembl_df["Class"]
 from transformers import RobertaTokenizer, RobertaModel, RobertaConfig
 import torch
 
-config = RobertaConfig.from_pretrained("./chemberta_saved_model/model")
+config = RobertaConfig.from_pretrained("./molbert_saved_model/model")
 config.output_hidden_states = True
 
 tok = RobertaTokenizer.from_pretrained("./robertatokenizer")
-model = RobertaModel.from_pretrained("./chemberta_saved_model/model", config=config)
+model = RobertaModel.from_pretrained("./molbert_saved_model/model", config=config)
 
 #List of Vectors
 #Embeds=[]
 Hiddens=[]
 Seqs=[]
 Class=[]
-Selfies=[]
 for selfies,classes in zip(selfies_df,class_df):
     try:
         sentence = torch.tensor([tok.encode(selfies)])
@@ -31,35 +30,48 @@ for selfies,classes in zip(selfies_df,class_df):
         #embedding_output = model.embeddings(sentence) same with hidden states
 
         sequence_output = output[0]
-        #print(embedding_output,"Seq\n",sequence_output)
-        #print(embedding_output.shape,sequence_output.shape)
         #Embeds.append(embedding_output.detach().numpy())
-        Hiddens.append(np.mean(hidden_output.detach().numpy()[0], axis=0)) #Tensor that requires grad
-        Seqs.append(np.mean(sequence_output.detach().numpy()[0], axis=0)) #(1,token size,768) to (768,)
+        Hiddens.append(np.mean(hidden_output.detach().numpy()[0], axis=0))#.reshape(-1, 1)) #Tensor that requires grad therefore use detach
+        Seqs.append(np.mean(sequence_output.detach().numpy()[0], axis=0))#.reshape(-1, 1)) #(1,token size,768) to (768,)
         Class.append(classes)
-        Selfies.append(selfies)
     except:
         #Do Nothing if Fails
         continue
 
-print(Hiddens[4][0])
-print(Seqs[4].shape) 
-
-#Create Dataframe of Data
+"""
+Check for requierements
 import pandas as pd
- 
-data = {'Sefies':Selfies,
-        'Hiddens':Hiddens,
-        'Seqs': Seqs,
-        'Class': Class}
+import datashader as ds
+import datashader.transfer_functions as tf
+import datashader.bundling as bd
+import matplotlib.pyplot as plt
+import colorcet as cl
+import matplotlib.colors as mlpcl
+import matplotlib.cm as matlpcm
+import bokeh.plotting as bpl
+import bokeh.transform as btr
+import holoviews as hv
+import holoviews.operation.datashader as hd
+"""
+#e=df["Seqs"].to_numpy().reshape(1, -1)
+#print(d.shape)
+#print(e.shape)
+#print(np.array(Seqs))
+#print(Seqs_arr.shape)
+#print(Seqs_arr[0].shape)
 
-df = pd.DataFrame(data)
+Seqs_arr = np.array(Seqs)
+Class_arr = np.array(Class)
+Hiddens_arr = np.array(Hiddens)
+
 
 #Umap Representation
-import umap
-mapper = umap.UMAP().fit(df[["Hiddens", "Class"]])
-umap.plot.points(mapper, labels=df["Class"])
+import umap.umap_ as umap
+import umap.plot
+mapper = umap.UMAP(random_state=123).fit(Hiddens_arr) #the clustering and values will be different, to keep the same graph set the common seed
+umap_p = umap.plot.points(mapper, labels=Class_arr)
+umap.plot.show(umap_p)
 
-mapper = umap.UMAP().fit(df[["Seqs", "Class"]])
-umap.plot.points(mapper, labels=df["Class"])
-
+mapper = umap.UMAP(random_state=123).fit(Seqs_arr)
+umap_p = umap.plot.points(mapper, labels=Class_arr)
+umap.plot.plt.show()
