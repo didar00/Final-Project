f0d2f2b5530b7e286cebc2be97c4973cb9bf9bca
didar00 <didartufekcigil@gmail.com>
2022-03-05 20:53:30 +0300

    classification with chemprop added

--- chemprop_test.py
+++ chemprop_test.py
@@ -1,47 +1,46 @@
 
-import chemprop
 
-# read dataset file
-# create a data (molecule) point for each
-# smiles representation
+def split_data(filename):
+    import chemprop
 
-with open("chemprop_data/bbbp.csv", "r") as f: # make this automatic
-    lines = f.readlines()[1:]
+    # read dataset file
+    # create a data (molecule) point for each
+    # smiles representation
 
-lines = list(map(str.strip, lines))
+    with open(filename, "r") as f: # make this automatic
+        lines = f.readlines()[1:]
 
-print("LINES")
-print(lines[0:10])
-print()
+    lines = list(map(str.strip, lines))
 
-molecule_list = list()
+    print("LINES")
+    print(lines[0:10])
+    print()
 
-for line in lines:
-    rep, target = line.split(",")[0], float(line.split(",")[1])
-    temp = list()
-    temp.append(rep)
+    molecule_list = list()
 
+    for line in lines:
+        rep, target = line.split(",")[0], float(line.split(",")[1])
+        temp = list()
+        temp.append(rep)
+        molecule = chemprop.data.data.MoleculeDatapoint(smiles=temp, targets=target)
+        molecule_list.append(molecule)
 
-    #molecule = chemprop.data.data.MoleculeDatapoint(smiles=list(rep), targets=ind)
-    molecule = chemprop.data.data.MoleculeDatapoint(smiles=temp, targets=target)
-    molecule_list.append(molecule)
+    print("MOLECULES")
+    print(molecule_list[0:10])
+    print()
 
-print("MOLECULES")
-print(molecule_list[0:10])
-print()
+    # load molecule list to the molecule dataset
+    # to use it in the scaffold split later
+    molecule_obj = chemprop.data.data.MoleculeDataset(molecule_list)
 
-# load molecule list to the molecule dataset
-# to use it in the scaffold split later
-molecule_obj = chemprop.data.data.MoleculeDataset(molecule_list)
+    # time to split the dataset using scaffold split
+    (train, val, test) = chemprop.data.scaffold.scaffold_split(data = molecule_obj, sizes = (0.8, 0.1, 0.1), seed = 42)
 
-# time to split the dataset using scaffold split
-(train, val, test) = chemprop.data.scaffold.scaffold_split(data = molecule_obj, sizes = (0.8, 0.1, 0.1), seed = 42)
+    print(val.atom_features_size())
+    print(val.smiles())
+    print("************************************")
+    print(test.targets())
 
-# it should be ready already
-# just tset it
+    return (train, val, test)
 
-print(val.atom_features_size())
-print(val.smiles())
-print("************************************")
-print(test.targets())
-
+split_data("chemprop_data/classification/bace.csv")
